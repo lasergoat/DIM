29ee78db04bb35a6f7a80d4b6d75a886f93ada22
Daniel Walker <polyesterhat@gmail.com>
2015-03-14 11:11:49 -0400

    fix this angular file also

--- src/app.config.js
+++ src/app.config.js
@@ -3,6 +3,39 @@
 (function () {
   'use strict';
 
+  function appRun(dimBungieService, dimUserSystemIds, dimConfig, $window) {
+    var platformData = null;
+    var storeData = null;
+
+    dimBungieService.loadBungieNetUser()
+      .then(function (data) {
+        var bungieUser = data.Response;
+
+        if (bungieUser.gamerTag) {
+          dimUserSystemIds.xbl = {
+            id: bungieUser.gamerTag,
+            type: 1,
+            label: 'Xbox'
+          };
+        }
+
+        if (bungieUser.psnId) {
+          dimUserSystemIds.psn = {
+            id: bungieUser.psnId,
+            type: 2,
+            label: 'PlayStation'
+          };
+        }
+
+        dimConfig.active = dimUserSystemIds.xbl;
+
+        // if (!_.isNull(dimUserSystemIds.psn.id))
+        //   dimConfig.active = dimUserSystemIds.psn;
+      });
+  }
+
+  appRun.$inject = ['dimBungieService', 'dimUserSystemIds', 'dimConfig', '$window'];
+
   angular.module('dimApp')
     .value('dimUserSystemIds', {
       xbl: null,
@@ -44,36 +77,4 @@
     })
     .run(appRun);
 
-  appRun.$inject = ['dimBungieService', 'dimUserSystemIds', 'dimConfig', '$window'];
-
-  function appRun(dimBungieService, dimUserSystemIds, dimConfig, $window) {
-    var platformData = null;
-    var storeData = null;
-
-    dimBungieService.loadBungieNetUser()
-      .then(function (data) {
-        var bungieUser = data.Response;
-
-        if (bungieUser.gamerTag) {
-          dimUserSystemIds.xbl = {
-            id: bungieUser.gamerTag,
-            type: 1,
-            label: 'Xbox'
-          };
-        }
-
-        if (bungieUser.psnId) {
-          dimUserSystemIds.psn = {
-            id: bungieUser.psnId,
-            type: 2,
-            label: 'PlayStation'
-          };
-        }
-
-        dimConfig.active = dimUserSystemIds.xbl;
-
-        // if (!_.isNull(dimUserSystemIds.psn.id))
-        //   dimConfig.active = dimUserSystemIds.psn;
-      });
-  }
 })();
